7b406a0e894be0b4107bcb287b0bc15d7e4a5698
Chaitanya Tandon <tandonchaitanya8.ct@gmail.com>
2024-12-06 00:03:00 +0530

    Analysis 1 added

--- utils/utils.py
+++ utils/utils.py
@@ -2,10 +2,17 @@ import json
 
 class FileLoader:
     
-    def __init__(self, file_path):
-        self.file_path = file_path
+    def __init__(self):
+        pass
     
-    def load_json_data(self):
+    def load_json_data(self,file_path):
+        self.file_path = file_path
+        with open(self.file_path, 'r') as file:
+               data = json.load(file)
+        return data
+    
+    def load_questions_data_mapping(self,file_path):
+        self.file_path = file_path
         with open(self.file_path, 'r') as file:
                data = json.load(file)
         return data

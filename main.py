8b0640d0df71fe6960b5c1230b0a9f135aad8e0f
Chaitanya Tandon <tandonchaitanya8.ct@gmail.com>
2024-12-09 01:02:29 +0530

    Logger added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from pyspark.sql import SparkSession
 from jobs.loader import Loader
 from jobs.jobbuilder import JobBuilder
 import os
+from jobs.logger import Logger
 
 
 
@@ -13,7 +14,10 @@ if __name__ == "__main__":
     conf_data = FileLoader().load_json_data("configs/config.json")
     question_data_mapping = FileLoader().load_json_data("configs/questions_data_mapping.json")
     question_data_mapping = question_data_mapping.get(str(conf_data["functions"]["analysis_id"]))
+    LOG = Logger("main",os.getcwd()+conf_data["logging_path"])
+    LOG._logger.info(conf_data)
     spark = SparkSession.builder.appName(str(question_data_mapping.get("app_name"))).getOrCreate()
+    LOG._logger.info(spark)
     result = JobBuilder(path=conf_data["file_path"]["source_path"],spark=spark,question_data_mapping=question_data_mapping). \
                         extractionData(questionId=conf_data["functions"]["analysis_id"])
     Loader(spark=spark).writeCsvFile(path=conf_data["file_path"]["destination_path"]+"Analysis " +
